3b6061e44553a4001d7633465b9e4bdd3525ea75
Guo, Li <28505129+iguoli@users.noreply.github.com>
2024-07-10 07:24:40 +0800

    Added which-key plugin

--- lua/plugins/not_vscode/utils.lua
+++ lua/plugins/not_vscode/utils.lua
@@ -20,5 +20,19 @@ return {
       vim.keymap.set("n", "<C-P>", require('fzf-lua').files, { desc = "Fzf Files" })
       vim.keymap.set("n", "<C-\\>", require('fzf-lua').buffers, { desc = "Fzf Buffers" })
     end
-  }
+  },
+
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    init = function()
+      vim.o.timeout = true
+      vim.o.timeoutlen = 300
+    end,
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    }
+  },
 }
